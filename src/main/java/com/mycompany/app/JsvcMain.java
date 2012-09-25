fd729993f6c69da787da7935d7750724037e3fc1
bergliang <config2005@yahoo.cn>
2012-09-25 16:12:40 +0800

    create

---
+++ src/main/java/com/mycompany/app/JsvcMain.java
@@ -0,0 +1,30 @@
+package com.mycompany.app;
+
+import org.apache.commons.daemon.Daemon;
+import org.apache.commons.daemon.DaemonContext;
+
+public class JsvcMain implements Daemon {
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void init(DaemonContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void start() throws Exception {
+		System.out.println("start....");
+		// TODO Auto-generated method stub
+
+	}
+
+	public void stop() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("stop....");
+
+	}
+
+}
